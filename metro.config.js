3992da2263c7ddd955e6700b68cd7fe3da387801
BatistaTony <batistatomeoliveira96@gmail.com>
2020-11-26 03:01:06 +0100

    new config

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,3 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 const {getDefaultConfig} = require('metro-config');
 
 module.exports = (async () => {
@@ -22,8 +15,9 @@ module.exports = (async () => {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
-      assetExts: assetExts.filter((ext: any) => ext !== 'svg'),
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
   };
 })();
+
